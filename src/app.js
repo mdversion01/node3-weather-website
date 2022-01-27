2dd0ac0fbe85777500cfdfed75437c922b528538
Mathew Daugherty <mathewdaugherty@mac.com>
2022-01-27 16:15:54 -0500

    fixed typo

--- src/app.js
+++ src/app.js
@@ -118,5 +118,5 @@ app.get('*', (req, res) => {
 })
 
 app.listen(port, () => {
-  console.log('Server is up on port '+ port + ''.')
+  console.log('Server is up on port ' + port)
 }); // <-- starts up the app and '3000' is the port
